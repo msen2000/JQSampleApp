d704dfa1a653fbd53e93fc6b572c0c0007d15c62
ca31085 <ca31085@CAL1100370.deltads.ent>
2013-10-11 01:00:44 -0700

    fd

---
+++ JQSampleApp/src/org/sen/jq/bean/AdminLoginBean.java
@@ -0,0 +1,38 @@
+package org.sen.jq.bean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class AdminLoginBean implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String userName;
+	private String password;
+	
+	public AdminLoginBean() {
+		
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
